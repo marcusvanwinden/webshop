29e6de7d561751dfaa7e9fa677dd1db2d533b5e6
Marcus van Winden <marcus.van.winden@icloud.com>
2021-05-14 17:43:03 +0200

    Save sessions in MongoDB store

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -3,14 +3,10 @@ const router = express.Router();
 const Products = require('../database/product');
 
 router.get('/login', async (req, res) => {
-  const products = await Products.getAllProducts();
-  console.log(products);
-  console.log(req.session.name);
   res.send('Login');
 });
 
 router.get('/register', (req, res) => {
-  req.session.name = 'Marcus';
   res.send('Register');
 });
 
